cf2d6e2c77b64e42dc9db9dedbcbc8343004c8a8
Yash Gusain <yashgusain6002@gmail.com>
2024-12-22 09:39:22 +0530

    Reformation[Home Page]
    HOME PAGE Reformation:
    1.Re-did the CSS-more efficient-more organised(style.css).
    2.Added hamburger menu bar(style.css).
    3.Made Home page compatible for ALL devices.

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector("#navbar");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll("#navbar li a").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}));
+
+
 function scrollLeft(sliderId) {
     const slider = document.getElementById(sliderId);
     if (slider) {
